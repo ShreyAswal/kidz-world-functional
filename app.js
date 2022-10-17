5c788b7dfc46ed5da22fbfb74f4f1bec204ae92c
Shrey Aswal <aswalshrey@gmail.com>
2022-10-18 05:44:03 +1300

    Update app.js

--- app.js
+++ app.js
@@ -1,142 +1,149 @@
-var cartValue = document.getElementById("cart-value");
-var cartButton = document.getElementById("cart");
+const btn=document.querySelectorAll('.button');
+const cartValue=document.getElementById('cart-value');
+const cart=document.getElementById('cart');
 
-var addButtons = document.getElementsByClassName("button");
+let bill="";
 
 var items = [
-  {
-    name: "This was our pact",
-    quantity: 0,
-    dollars: 7,
-    cents: 49,
-  },
-  {
-    name: "The famous five",
-    quantity: 0,
-    dollars: 4,
-    cents: 59,
-  },
-  {
-    name: "Matilda",
-    quantity: 0,
-    dollars: 6,
-    cents: 80,
-  },
-  {
-    name: "Harry Potter",
-    quantity: 0,
-    dollars: 10,
-    cents: 0,
-  },
-  {
-    name: "For Young Readers",
-    quantity: 0,
-    dollars: 7,
-    cents: 29,
-  },
-  {
-    name: "Wimpy Kid - DIY",
-    quantity: 0,
-    dollars: 4,
-    cents: 99,
-  },
-  {
-    name: "Dart Board",
-    quantity: 0,
-    dollars: 17,
-    cents: 49,
-  },
-  {
-    name: "Connect Four",
-    quantity: 0,
-    dollars: 19,
-    cents: 99,
-  },
-  {
-    name: "Jenga",
-    quantity: 0,
-    dollars: 20,
-    cents: 99,
-  },
-  {
-    name: "Monopoly",
-    quantity: 0,
-    dollars: 19,
-    cents: 49,
-  },
-  {
-    name: "Bookmarks",
-    quantity: 0,
-    dollars: 12,
-    cents: 49,
-  },
-  {
-    name: "Birthday Card",
-    quantity: 0,
-    dollars: 12,
-    cents: 49,
-  },
-  {
-    name: "Stuffed toys",
-    quantity: 0,
-    dollars: 15,
-    cents: 99,
-  },
-  {
-    name: "Dream catcher drawing",
-    quantity: 0,
-    dollars: 18,
-    cents: 49,
-  },
-];
+    {
+      name: "This was our pact",
+      quantity: 0,
+      dollars: 7,
+      cents: 49,
+    },
+    {
+      name: "The famous five",
+      quantity: 0,
+      dollars: 4,
+      cents: 59,
+    },
+    {
+      name: "Matilda",
+      quantity: 0,
+      dollars: 6,
+      cents: 80,
+    },
+    {
+      name: "Harry Potter",
+      quantity: 0,
+      dollars: 10,
+      cents: 0,
+    },
+    {
+      name: "For Young Readers",
+      quantity: 0,
+      dollars: 7,
+      cents: 29,
+    },
+    {
+      name: "Wimpy Kid - DIY",
+      quantity: 0,
+      dollars: 4,
+      cents: 99,
+    },
+    {
+      name: "Dart Board",
+      quantity: 0,
+      dollars: 17,
+      cents: 49,
+    },
+    {
+      name: "Connect Four",
+      quantity: 0,
+      dollars: 19,
+      cents: 99,
+    },
+    {
+      name: "Jenga",
+      quantity: 0,
+      dollars: 20,
+      cents: 99,
+    },
+    {
+      name: "Monopoly",
+      quantity: 0,
+      dollars: 19,
+      cents: 49,
+    },
+    {
+      name: "Bookmarks",
+      quantity: 0,
+      dollars: 12,
+      cents: 49,
+    },
+    {
+      name: "Birthday Card",
+      quantity: 0,
+      dollars: 12,
+      cents: 49,
+    },
+    {
+      name: "Stuffed toys",
+      quantity: 0,
+      dollars: 15,
+      cents: 99,
+    },
+    {
+      name: "Dream catcher drawing",
+      quantity: 0,
+      dollars: 18,
+      cents: 49,
+    },
+  ];
 
-function updateCart() {
-  let cart = 0;
-  for (index = 0; index < items.length; index++) {
-    cart = cart + items[index].quantity;
-  }
-  cartValue.innerHTML = cart;
-}
-
-for (let i = 0; i < addButtons.length; i++) {
-  addButtons[i].onclick = (e) => {
-    items[i].quantity++;
-    updateCart();
-  };
-}
-
-var finalDollars = 0;
-var finalCents = 0;
-
-function updatePrice() {
-  let totalPriceInCents = 0;
-
-  for (index = 0; index < items.length; index++) {
-    totalPriceInCents =
-      totalPriceInCents +
-      items[index].quantity * (items[index].dollars * 100 + items[index].cents);
-  }
-  finalDollars = Math.floor(totalPriceInCents / 100);
-  finalCents = totalPriceInCents % 100;
-}
-
-
-cartButton.onclick = () => {
-  updatePrice();
-
-
-  for (let index = 0; index < items.length; index++) {
-    if (items[index].quantity != 0) {
-      console.log(
-        "Item name: " +
-          items[index].name +
-          " - Quantity: " +
-          items[index].quantity
-      );
+function displayCount() {
+    let itemCount=0;
+    for(let i=0;i<items.length;i++) {
+        itemCount+= items[i].quantity;
     }
-  }
+cartValue.innerText=itemCount;
+}
 
-  console.log(
-    "The total amount is " + finalDollars + "$ and " + finalCents + " cents"
-  );
-};
+for (let i=0;i<btn.length;i++) {
+    btn[i].onclick=()=>{
+        items[i].quantity++;
+        displayCount();
+    }
+}
+
+let totalCents=0;
+let totalDollars=0;
+
+function countPrice() {
+    let cents=0;
+    for (let i=0;i<items.length;i++) {
+        cents+= items[i].quantity*(items[i].dollars*100+items[i].cents);
+    }
+    totalDollars=Math.floor(cents/100);
+    totalCents=cents%100;
+}
+var whatsAppLink="https://api.whatsapp.com/send?phone=918130025885&text=%20*YOURMESSAGE*";
+
+function whatsapp(){
+    for(i=0;i<items.length;i++)
+    {
+        if (items[i].quantity !=0)
+        {
+            whatsAppLink+="%0A" + items[i].name + "%20" + items[i].quantity;
+        }
+    }
+
+    whatsAppLink+="%0A" + "Total%20Price:%20" + totalDollars + "%20" + totalCents + "c";
+}
+
+cart.onclick=()=>{
+    countPrice();
+    whatsapp();
+    window.open(whatsAppLink, "_blank");
+    for (let i=0;i<items.length;i++) 
+    {
+        if (items[i].quantity!=0) 
+        {
+            bill+=`Items name: ${items[i].name} - Quantity: ${items[i].quantity} \n`;
+        }
+    }
+
+    bill+= `\n-------------------------------------\nThe total amount is ${totalDollars} $ and ${totalCents} cents`;
+
+    console.log(bill);
+}
